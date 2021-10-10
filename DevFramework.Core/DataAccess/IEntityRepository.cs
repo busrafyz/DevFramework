79badc6bb71a325ee422945d2f054362b6af577a
busrafyz <78544700+busrafyz@users.noreply.github.com>
2021-10-11 00:04:44 +0300

    IEntityRepository ve IQueryableRepository arayüzlerinin oluşturulması

---
+++ DevFramework.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿using DevFramework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DevFramework.Core.DataAccess
+{
+    public interface IEntityRepository<T> where T:class, IEntity, new() 
+    {
+        List<T> GetList(Expression<Func<T, bool>> filter=null);
+        T Get(T Entity);
+        T Update(T Entity);
+        void Delete(T Entity);
+    }
+}
---
+++ DevFramework.Core/DataAccess/IQueryableRepository.cs
@@ -0,0 +1,12 @@
+﻿using DevFramework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFramework.Core.DataAccess
+{
+    interface IQueryableRepository<T> where T: class, IEntity, new()
+    {
+        IQueryableRepository<T> Table { get; }
+    }
+}
